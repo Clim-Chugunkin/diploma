2da2a7aaf2f1d706c20065ddd3f1f779b332a225
Kislev Sergey <mr.sergey.kiselev.85@mail.ru>
2026-02-03 19:22:53 +0700

    stats-serv + client

--- stat-service/service/src/main/java/ru/practicum/repository/EndpointRepository.java
+++ stat-service/service/src/main/java/ru/practicum/repository/EndpointRepository.java
@@ -11,17 +11,19 @@ import java.util.List;
 public interface EndpointRepository extends JpaRepository<EndpointHit, Long> {
 
     @Query("select new ru.practicum.DTO.ViewStats(" +
-            "it.app, it.uri, CASE WHEN ?3 = true THEN count(distinct(it.ip)) ELSE count(*) END ) from EndpointHit as it " +
+            "it.app, it.uri, CASE WHEN ?3 = true THEN count(distinct(it.ip)) ELSE count(*) END  as cnt )  from EndpointHit as it " +
             "where it.timestamp between ?1 and ?2 " +
-            "group by it.app, it.uri")
+            "group by it.app, it.uri " +
+            "order by cnt desc")
     List<ViewStats> getStatistic(LocalDateTime start, LocalDateTime stop, boolean unique);
 
 
     @Query("select new ru.practicum.DTO.ViewStats(" +
-            "it.app, it.uri, CASE WHEN ?4 = true THEN count(distinct(it.ip)) ELSE count(*) END ) from EndpointHit as it " +
+            "it.app, it.uri, CASE WHEN ?4 = true THEN count(distinct(it.ip)) ELSE count(*) END as cnt )  from EndpointHit as it " +
             "where it.timestamp between ?1 and ?2 " +
             "and it.uri in ?3 " +
-            "group by it.app, it.uri")
+            "group by it.app, it.uri " +
+            "order by cnt desc")
     List<ViewStats> getStatistic(LocalDateTime start, LocalDateTime stop, String[] uris, boolean unique);
 
 }
