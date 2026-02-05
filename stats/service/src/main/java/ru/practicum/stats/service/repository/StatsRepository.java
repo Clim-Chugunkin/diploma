1e2a5ad6751607c5c1e0a0cef743c3938eec4e83
Kislev Sergey <mr.sergey.kiselev.85@mail.ru>
2026-02-05 17:31:26 +0700

    исправлены замечания

--- stats/service/src/main/java/ru/practicum/stats/service/repository/StatsRepository.java
+++ stats/service/src/main/java/ru/practicum/stats/service/repository/StatsRepository.java
@@ -9,15 +9,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StatsRepository extends JpaRepository<StatsHit, Long> {
-    @Query("select new ru.practicum.DTO.ViewStats(" +
-            "it.app, it.uri, CASE WHEN ?3 = true THEN count(distinct(it.ip)) ELSE count(*) END  as cnt )  from EndpointHit as it " +
+    @Query("select new ru.practicum.stats.DTO.ViewStats(" +
+            "it.app, it.uri, CASE WHEN ?3 = true THEN count(distinct(it.ip)) ELSE count(*) END  as cnt )  from StatsHit as it " +
             "where it.timestamp between ?1 and ?2 " +
             "group by it.app, it.uri " +
             "order by cnt desc")
     List<ViewStats> getStatistic(LocalDateTime start, LocalDateTime stop, boolean unique);
 
-    @Query("select new ru.practicum.DTO.ViewStats(" +
-            "it.app, it.uri, CASE WHEN ?4 = true THEN count(distinct(it.ip)) ELSE count(*) END as cnt )  from EndpointHit as it " +
+    @Query("select new ru.practicum.stats.DTO.ViewStats(" +
+            "it.app, it.uri, CASE WHEN ?4 = true THEN count(distinct(it.ip)) ELSE count(*) END as cnt )  from StatsHit as it " +
             "where it.timestamp between ?1 and ?2 " +
             "and it.uri in ?3 " +
             "group by it.app, it.uri " +
