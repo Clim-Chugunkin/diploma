90f618ecc25bbdeff5aaaa9cadc6d13a9720d186
Kislev Sergey <mr.sergey.kiselev.85@mail.ru>
2026-03-05 20:21:11 +0700

    comments

---
+++ main-server/src/main/java/ru/practicum/ewm/server/comment/model/Comment.java
@@ -0,0 +1,28 @@
+package ru.practicum.ewm.server.comment.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import ru.practicum.ewm.server.event.model.Event;
+import ru.practicum.ewm.server.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments", schema = "public")
+@Getter
+@Setter
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+    @Column(name = "comment")
+    private String comment;
+    private LocalDateTime created;
+    @ManyToOne
+    @JoinColumn(name = "author")
+    private User author;
+}
